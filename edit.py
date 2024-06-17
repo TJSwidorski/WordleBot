2dbd11debe1b72646e2e8cd665b709570c0e3844
tjs322 <tjs322@cornell.edu>
2024-06-17 16:43:23 -0400

    Simple duplicate letter fix

--- edit.py
+++ edit.py
@@ -2,14 +2,19 @@ class EditDictionary():
   def __init__(self, word_dict):
     self.__word_dict = word_dict
     self.__removed_letters = []
+    self.__required_letters = []
     
   def remove_letter(self, letter):
-      self.__removed_letters.append(letter)
-      words_to_remove = [word for word in self.__word_dict if letter in word]
-      for word in words_to_remove:
+      if letter in self.__required_letters:
+        print("Duplicate Letter")
+      else:
+        self.__removed_letters.append(letter)
+        words_to_remove = [word for word in self.__word_dict if letter in word]
+        for word in words_to_remove:
           self.__word_dict.pop(word)
   
   def require_correct_location(self, letter, location):
+    self.__required_letters.append(letter)
     words_to_remove = []
     for word in self.__word_dict:
       if word[location] != letter:
@@ -22,6 +27,7 @@ class EditDictionary():
 
 
   def require_wrong_location(self, letter, location):
+    self.__required_letters.append(letter)
     words_to_remove = []
     for word in self.__word_dict:
       if word[location] == letter:
@@ -39,5 +45,6 @@ class EditDictionary():
      return self.__removed_letters
   
   def best_word(self):
-    word_list = list(self.__word_dict)
-    return word_list[0]
+    if self.__word_dict:
+      return next(iter(self.__word_dict))
+    else: return None
