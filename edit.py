fd082fbf3c3109c60e4ee8a668dd64921e9f6d6f
tjs322 <tjs322@cornell.edu>
2024-06-17 16:22:22 -0400

    Implement Terminal Prompting

--- edit.py
+++ edit.py
@@ -1,40 +1,43 @@
 class EditDictionary():
   def __init__(self, word_dict):
-    self.word_dict = word_dict
+    self.__word_dict = word_dict
+    self.__removed_letters = []
     
   def remove_letter(self, letter):
-      words_to_remove = [word for word in self.word_dict if letter in word]
+      self.__removed_letters.append(letter)
+      words_to_remove = [word for word in self.__word_dict if letter in word]
       for word in words_to_remove:
-          self.word_dict.pop(word)
+          self.__word_dict.pop(word)
   
   def require_correct_location(self, letter, location):
     words_to_remove = []
-    for word in self.word_dict:
-      if word[location - 1] != letter:
+    for word in self.__word_dict:
+      if word[location] != letter:
         words_to_remove.append(word)
       elif letter not in word:
          words_to_remove.append(word) 
     
     for word in words_to_remove:
-       self.word_dict.pop(word)
+       self.__word_dict.pop(word)
 
 
   def require_wrong_location(self, letter, location):
     words_to_remove = []
-    for word in self.word_dict:
-      if word[location - 1] == letter:
+    for word in self.__word_dict:
+      if word[location] == letter:
         words_to_remove.append(word)
       elif letter not in word:
           words_to_remove.append(word) 
     
     for word in words_to_remove:
-        self.word_dict.pop(word)
+        self.__word_dict.pop(word)
 
   def return_dict(self):
-    return self.word_dict
+    return self.__word_dict
+  
+  def return_removed_letters(self):
+     return self.__removed_letters
   
   def best_word(self):
-    word_list = list(self.word_dict)
+    word_list = list(self.__word_dict)
     return word_list[0]
-
-
--- main.py
+++ main.py
@@ -1,8 +1,55 @@
-from database import *
+import database
+import edit
+import counter
+
 import sqlite3
 
-CONN = sqlite3.connect('WordleDictionary.db')
+words_dict = counter.final_word_dict
 
-words_list = WordleDatabase.retrieve_list(CONN, 'Words List')
+words_dict = edit.EditDictionary(words_dict)
 
-print(len(words_list))
+def check_win(color_list):
+  color_set = set(color_list)
+  return (len(color_set) == 1) and (color_set.pop() == 'green' or 'g')
+
+def iterate_dict(words_dict, word, removed_letters):
+  color_list = []
+  for i in range(5):
+    n = i + 1
+    print("Valid Options: \'Green\' (G), \'Yellow\' (Y), or \'Black\' (B)")
+    color = input(f'Letter {n} Color Results: ')
+    if (color.lower() == 'green') or (color.lower() == 'g'):
+      color_list.append(color.lower())
+      words_dict.require_correct_location(word[i], i)
+    elif (color.lower() == 'yellow') or (color.lower() == 'y'):
+      color_list.append(color.lower())
+      words_dict.require_wrong_location(word[i], i)
+    elif (color.lower() == 'black') or (color.lower() == 'b'):
+      color_list.append(color.lower())
+      if word[i] not in removed_letters:
+        words_dict.remove_letter(word[i])
+
+  return check_win(color_list)
+  
+
+  
+if __name__ == '__main__':
+  print('\nHello and welcome to WordleBot!\n')
+  best_word = words_dict.best_word()
+  print(f'The best first word is: {best_word}\n')
+
+  game_over = False
+  i = 0
+  while (game_over is False) or (i == 6):
+    i += 1
+    removed_letters = words_dict.return_removed_letters()
+    print('Removed letters: ', removed_letters)
+    best_word = words_dict.best_word()
+    print('Now, please input the results.\n')
+    game_over = iterate_dict(words_dict, best_word, removed_letters)
+    if game_over:
+      print(f'\n\n\nCongrats on getting the wordle in {i}!')
+    else:
+      print('\n\n\nThe next best word is: ', words_dict.best_word())
+    
+  print('\n\nThank you for using WordleBot!')
